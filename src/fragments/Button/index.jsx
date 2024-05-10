84549a3eb8dc8e621d0cbaef8ccbe3d469b4815b
Mxt@DEV <maxta.gamer@gmail.com>
2024-05-09 22:35:31 -0300

    Entrega-kenzie-hub update

--- src/fragments/Button/index.jsx
+++ src/fragments/Button/index.jsx
@@ -1,12 +1,11 @@
 import styles from './styles.module.scss';
 
-export const Button = ({ type, title, disabled, bgColor, navTo }) => {
+export const Button = ({ type, title, disabled, bgColor }) => {
   return (
     <button
       type={type}
       disabled={disabled}
       className={`${styles.btn} ${bgColor}`}
-      onClick={navTo}
     >
       {title}
     </button>
