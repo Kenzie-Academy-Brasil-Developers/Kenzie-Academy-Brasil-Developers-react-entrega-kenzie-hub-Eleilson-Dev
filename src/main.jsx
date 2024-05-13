f510d3af00faba43d8609190dbb2cae92aa28c79
Mxt@DEV <maxta.gamer@gmail.com>
2024-05-13 19:54:57 -0300

    Entrega-kenzie-hub update

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import App from './App.jsx';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { RegisterProvider } from './providers/RegisterContext.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RegisterProvider>
-        <App />
-      </RegisterProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <RegisterProvider>
+          <App />
+        </RegisterProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
